8a4586dfff783ebe300b8c5a2fa9f479fbd4252a
turner-white <turnerwhite@me.com>
2022-04-21 17:54:40 -0400

    autograder!

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ server.js [options]
 
 --help	Return this message and exit.
 `)
-// If --help or -h, echo help text to STDOUT and exit
+// If --help or -h, echo help text to STDOUT and exitff
 if (args.help || args.h) {
     console.log(help)
     process.exit(0)
